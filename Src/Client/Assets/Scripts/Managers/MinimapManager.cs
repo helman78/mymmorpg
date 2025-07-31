f5d95072639732b4bbfb04221d3cbfe09769df70
helman78 <bjfu_helman@163.com>
2025-07-31 15:33:43 +0800

    提交1

---
+++ Src/Client/Assets/Scripts/Managers/MinimapManager.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Managers
+{
+    class MinimapManager : MonoSingleton<MinimapManager>
+    {
+
+        public UIMinimap minimap;
+        private Collider minimapBoundingBox;
+        public Collider MinimapBoundingBox
+        {
+            get { return this.minimapBoundingBox; }
+        }
+        public Sprite LoadCurMiniMap()
+        {
+            return Resloader.Load<Sprite>("UI/Minimap/" + User.Instance.CurrentMapData.MiniMap);
+        }
+        public void UpdateMinimap(Collider minimapBoundingBox)
+        {
+            this.minimapBoundingBox = minimapBoundingBox;
+            if (this.minimap != null)
+                this.minimap.UpdateMinimap();
+        }
+    }
+}
---
+++ Src/Client/Assets/Scripts/Managers/NpcManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Data;
+
+namespace Managers
+{
+    class NPCManager : Singleton<NPCManager>
+    {
+        public delegate bool NPCActionHandler(NPCDefine npc);
+        Dictionary<NpcFunction, NPCActionHandler> eventMap = new Dictionary<NpcFunction, NPCActionHandler>();
+
+        public void RegisterNPCEvent(NpcFunction function, NPCActionHandler action)
+        {
+            if (!eventMap.ContainsKey(function))
+            {
+                eventMap[function] = action;
+            }
+            else eventMap[function] += action;
+        }
+        public NPCDefine GetNPCDefine(int npcId)
+        {
+            return DataManager.Instance.NPCs[npcId];
+        }
+        public bool Interactive(int npcId)
+        {
+            if (DataManager.Instance.NPCs.ContainsKey(npcId))
+            {
+                var npc = DataManager.Instance.NPCs[npcId];
+                return Interactive(npc);
+            }
+            return false;
+        }
+        public bool Interactive(NPCDefine npc)
+        {
+            if(npc.Type == NpcType.Task)
+            {
+                return DoTaskInteractive(npc);
+            }
+            else if(npc.Type == NpcType.Functional)
+            {
+                return DoFunctionInteractive(npc);
+            }
+            return false;
+        }
+        private bool DoTaskInteractive(NPCDefine npc)
+        {
+            MessageBox.Show("点击了NPC: " + npc.Name, "NPC对话");
+            return true;
+        }
+        private bool DoFunctionInteractive(NPCDefine npc)
+        {
+            if (npc.Type != NpcType.Functional)
+                return false;
+            if (!eventMap.ContainsKey(npc.Function))
+                return false;
+            return eventMap[npc.Function](npc);
+        }
+    }
+}
