2ed1aa0c71c408b88860d4a91c52afd9e128f8b6
helman78 <bjfu_helman@163.com>
2025-08-06 23:14:59 +0800

    部分未添加的添加一下

---
+++ Src/Client/Assets/Scripts/Managers/ShopManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Data;
+using Services;
+
+namespace Managers
+{
+    class ShopManager : Singleton<ShopManager>
+    {
+        public void Init()
+        {
+            NPCManager.Instance.RegisterNPCEvent(NpcFunction.InvokeShop, OnOpenShop);
+        }
+        private bool OnOpenShop(NPCDefine npc)
+        {
+            this.ShowShop(npc.Param);
+            return true;
+        }
+        public void ShowShop(int shopId)
+        {
+            ShopDefine shop;
+            if(DataManager.Instance.Shops.TryGetValue(shopId, out shop))
+            {
+                UIShop uiShop = UIManager.Instance.Show<UIShop>();
+                if(uiShop != null)
+                {
+                    uiShop.SetShop(shop);
+                }
+            }
+        }
+        public bool BuyItem(int shopId, int shopItemId)
+        {
+            ItemService.Instance.SendBuyItem(shopId, shopItemId);
+            return true;
+        }
+    }
+}
